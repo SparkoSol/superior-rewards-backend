5a723c928d1bd25c90ef90b1d03a82b1aecb5f00
Arish Sultan <arishgit000@gmail.com>
2024-09-11 15:16:58 +0500

    fix: update welcome message on index.

--- src/app/app.service.ts
+++ src/app/app.service.ts
@@ -3,6 +3,6 @@ import { Injectable } from '@nestjs/common';
 @Injectable()
 export class AppService {
     getHello(): string {
-        return 'Hello World!';
+        return `Superior Gas reward backend is running...🎉🎉🎉, Port:[${process.env.APP_PORT}] & Environment:[${process.env.NODE_ENVIRONMENT}]`;
     }
 }
