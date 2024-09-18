8f02397f0a28546c358c01ef40508f70033676cf
Rehman Ahmed Khan <rehmankha73@gmail.com>
2024-09-18 17:46:15 +0500

    feat!: update exipration time of jwt token.

--- src/modules/auth/auth.module.ts
+++ src/modules/auth/auth.module.ts
@@ -14,7 +14,7 @@ import { jwtConstants } from './constants';
         PassportModule,
         JwtModule.register({
             secret: jwtConstants.secret,
-            signOptions: { expiresIn: '60m' },
+            signOptions: { expiresIn: '1d' },
         }),
     ],
     controllers: [AuthController],
