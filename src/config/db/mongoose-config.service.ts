450d5db02e3907358167fd48a1acafa4544e8f6a
Rehman Ahmed Khan <rehmankha73@gmail.com>
2024-09-26 11:40:49 +0500

    Update mongoose-config.service.ts

--- src/config/db/mongoose-config.service.ts
+++ src/config/db/mongoose-config.service.ts
@@ -7,7 +7,6 @@ export class MongooseConfigService implements MongooseOptionsFactory {
     createMongooseOptions(): MongooseModuleOptions {
         return {
             uri: process.env.MONGO_URI,
-            autoIndex: true,
         };
     }
 }
