48fafa5efc555ddb50b0fac602d77e93007a5342
Rehman Ahmed Khan <rehmankha73@gmail.com>
2024-10-28 17:19:18 +0500

    Update app.service.ts

--- src/app/app.service.ts
+++ src/app/app.service.ts
@@ -4,6 +4,6 @@ import * as process from 'process';
 @Injectable()
 export class AppService {
     getHello(): string {
-        return `Welcome! Superior Backend is running on Port:[${process.env.port}], & Environment:[${process.env.NODE_ENVIRONMENT}]... 😊🎉`;
+        return `Welcome! Superior Backend is running on Port:[${process.env.APP_PORT}], & Environment:[${process.env.NODE_ENVIRONMENT}]... 😊🎉`;
     }
 }
